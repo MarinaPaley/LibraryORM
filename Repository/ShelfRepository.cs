76462bb7e0309a3ebd3c20a755e5f6b469c6e3d5
MarinaPaley <marina_paley@mail.ru>
2024-11-29 14:14:42 +0300

    Выделен базовый класс

--- Repository/ShelfRepository.cs
+++ Repository/ShelfRepository.cs
@@ -14,7 +14,7 @@ namespace Repository
     /// <summary>
     /// Репозиторий для класса <see cref="Domain.Shelf"/>.
     /// </summary>
-    public sealed class ShelfRepository
+    public sealed class ShelfRepository : ShelfRepositoryBase
     {
         /// <summary>
         /// Инициализирует новый экземпляр класса <see cref="ShelfRepository"/>.
@@ -27,64 +27,5 @@ namespace Repository
         {
             this.DataContext = dataContext ?? throw new ArgumentNullException(nameof(dataContext));
         }
-
-        /// <summary>
-        /// Контекст доступа к данным.
-        /// </summary>
-        public DataContext DataContext { get; }
-
-        /// <summary>
-        /// Создает полку.
-        /// </summary>
-        /// <param name="entity">Полка.</param>
-        /// <returns>Контекст доступа к сущности Полка.</returns>
-        public Shelf Create(Shelf entity) => this.DataContext.Add(entity).Entity;
-
-        /// <summary>
-        /// Изменяет полку.
-        /// </summary>
-        /// <param name="shelf">Полка.</param>
-        /// <returns>Измененный контекст доступа к сущности Полка.</returns>
-        public Shelf Update(Shelf shelf) => this.DataContext.Update(shelf).Entity;
-
-        /// <summary>
-        /// Удаляет полку.
-        /// </summary>
-        /// <param name="shelf">Полка.</param>
-        /// <returns>Измененный контекст доступа к сущности Полка.</returns>
-        public Shelf Delete(Shelf shelf) => this.DataContext.Remove(shelf).Entity;
-
-        /// <summary>
-        /// Сохраняет контекст в БД.
-        /// </summary>
-        /// <returns>Количество измененных сущностей.</returns>
-        public int Save() => this.DataContext.SaveChanges();
-
-        /// <summary>
-        /// Получение всех полок.
-        /// </summary>
-        /// <returns> Множество (<see cref="IQueryable{Shelf}"/>) всех полок.</returns>
-        public IQueryable<Shelf> GetAll() => this.DataContext.Shelves.IgnoreAutoIncludes();
-
-        /// <summary>
-        /// Получение конкретной полки по её идентификатору.
-        /// </summary>
-        /// <param name="id">Идентификатор полки.</param>
-        /// <returns>Полка.</returns>
-        public Shelf? Get(Guid id) => this.GetAll().SingleOrDefault(entity => entity.Id == id);
-
-        /// <summary>
-        /// Поиск полки по предикату (<paramref name="predicate"/>).
-        /// </summary>
-        /// <param name="predicate">Предикат, которому должна удовлетворять полка.</param>
-        /// <returns>Полка или <see langword="null"/>.</returns>
-        public Shelf? Find(Expression<Func<Shelf, bool>> predicate) => this.GetAll().FirstOrDefault(predicate);
-
-        /// <summary>
-        /// Поиск множества полок по предикату (<paramref name="predicate"/>).
-        /// </summary>
-        /// <param name="predicate">Предикат, которому должна удовлетворять полка.</param>
-        /// <returns>Множество (<see cref="IQueryable{Shelf}"/>) всех полок.</returns>
-        public IQueryable<Shelf> Filter(Expression<Func<Shelf, bool>> predicate) => this.GetAll().Where(predicate);
     }
 }
